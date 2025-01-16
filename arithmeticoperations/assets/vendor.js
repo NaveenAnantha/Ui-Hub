fe1d0369d6de0284df272368e86d69e4af48162f
Naveen Anantha <118650957+NaveenAnantha@users.noreply.github.com>
2025-01-16 18:11:54 +0530

    Update vendor.js

--- arithmeticoperations/assets/vendor.js
+++ arithmeticoperations/assets/vendor.js
@@ -14,10 +14,10 @@ const divideBtn = document.getElementById('btn-divide');
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
-// function outputResult(result, text) {
-//   currentResultOutput.textContent = result;
-//   currentCalculationOutput.textContent = text;
-// }
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+}
 // Addition
 addBtn.addEventListener("click",()=>{
   
