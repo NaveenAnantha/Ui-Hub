d150c8e87a69e2539f39f0432b64ed371ebf4a36
NaveenAnantha <ananthanaveen485@gmail.com>
2025-01-21 16:24:09 +0530

    Arithmetic operations

--- arithmeticoperations/assets/vendor.js
+++ arithmeticoperations/assets/vendor.js
@@ -10,6 +10,7 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+const moduloBtn = document.getElementById('btn-modulo');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
@@ -45,4 +46,11 @@ divideBtn.addEventListener("click",()=>{
   let result=Number(userInput1.value)/Number(userInput2.value)
   currentResultOutput.innerHTML=result;
   currentCalculationOutput.innerHTML=`${Number(userInput1.value)}/${Number(userInput2.value)}`;
+})
+// Division
+moduloBtn.addEventListener("click",()=>{
+
+  let result=Number(userInput1.value)%Number(userInput2.value)
+  currentResultOutput.innerHTML=result;
+  currentCalculationOutput.innerHTML=`${Number(userInput1.value)}%${Number(userInput2.value)}`;
 })
